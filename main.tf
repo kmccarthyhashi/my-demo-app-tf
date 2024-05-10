fd1de0707d644e414e382cb250cc98c5775b7ffd
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-05-10 13:34:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ locals {
 }
 
 module "vpc" {
-  source  = "app.terraform.io/jacobbuckles-org/vpc/aws"
+  source  = "app.terraform.io/KELLY-training/vpc/aws"
   version = "0.0.2"
 
   cidr_vpc = "10.1.0.0/16"
