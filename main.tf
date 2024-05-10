30e64604fb1d31dac36ddc94a913ab36d5fe5b45
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-05-10 13:54:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ terraform {
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55.0"
+      version = "~> 4.42.0"
     }
   }
-  required_version = ">= 1.4.0"
+  required_version = ">= 0.14.5"
 }
 
 provider "aws" {
@@ -26,7 +26,7 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
+  version = "5.6.0"
 
   cidr_vpc = "10.1.0.0/16"
   cidr_subnet = "10.1.0.0/24"
