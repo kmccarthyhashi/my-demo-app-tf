14fb368badf79bc7033b19797cb9261b6bbf1093
Kelly McCarthy <kmccarthy@hashicorp.com>
2024-10-22 09:37:19 -0500

    remove xtra server

--- main.tf
+++ main.tf
@@ -60,15 +60,3 @@ resource "aws_instance" "ubuntu" {
 
   tags = local.tags
 }
-
-# new comment
-
-resource "aws_instance" "new" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
-  subnet_id                   = module.vpc.subnet_id
-  vpc_security_group_ids      = [module.vpc.vpc_security_group_id]
-  associate_public_ip_address = true
-
-  tags = local.tags
-}
